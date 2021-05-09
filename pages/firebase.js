8a605e394854d79ee124209ce1fd9f946341ccaa
Ashar Farooq <ashar744@gmail.com>
2021-05-09 00:31:32 -0400

    updated input page

---
+++ pages/firebase.js
@@ -0,0 +1,14 @@
+  import Firebase from 'firebase';
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyDLypzy5Iago3tpvyhg33taVSRL9kaXWMo",
+    authDomain: "fireroad053.firebaseapp.com",
+    databaseURL: "https://fireroad053-default-rtdb.firebaseio.com",
+    projectId: "fireroad053",
+    storageBucket: "fireroad053.appspot.com",
+    messagingSenderId: "866423312668",
+    appId: "1:866423312668:web:5fa84dadb0e6e393e9ead9",
+    measurementId: "G-W64TDQZZNZ"
+  };
+
+  Firebase.initializeApp(firebaseConfig);
